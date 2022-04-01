2c1eb0780ed5063caf7e9ed37734d7e9bd17d161
Maxim Krivchikov <maxim.krivchikov@gmail.com>
2022-04-01 10:28:19 +0000

    utf8 fix

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #define LIM 128
 #define UTF 4
+#define FIRST_TWOBYTE 0xC0
+#define FIRST_THREEBYTE 0xE0
+#define FIRST_FORUBYTE 0xF0
 
 #include <stdio.h>
 
@@ -10,63 +13,63 @@
 // (кончился поток ввода)
 // возвращает 1 и записывает ответ по указателю result,
 // если считать удалось
-int read_next(FILE *input, unsigned char *result);
+int read_next(FILE *input, unsigned char result[UTF]);
 
 int main(void)
 {
-    
+
     unsigned char first[UTF];
     unsigned char second[UTF];
 
     while (1) {
-        if (read_next(stdin, &first) == EOF) {
+        if (read_next(stdin, first) == EOF) {
             break;
         }
 
-        if (read_next(stdin, &second) == EOF) {
-            fputs(first, stdout);
+        if (read_next(stdin, second) == EOF) {
+            fputs((char *)first, stdout);
             break;
         }
 
-        fputs(second, stdout);
-        fputs(first, stdout);
+        fputs((char *)second, stdout);
+        fputs((char *)first, stdout);
     }
 
     return 0;
 }
 
-int read_next(FILE *input, unsigned char *result)
+int read_next(FILE *input, unsigned char result[UTF])
 {
-    for (int i = 0; i < UTF; i++) {
-        *(result+i) = 0;
-    }
     while (1) {
+        for (int i = 0; i < UTF; i++) {
+            *(result + i) = 0;
+        }
         int text = fgetc(input);
         if (text >= LIM) {
             // 1100 0000
-            if ( text < 0xC0) {
+            if (text < FIRST_TWOBYTE) {
                 continue;
             }
             *result = (unsigned char)text;
             int skip = 0;
             int count = 0;
-            if (text >= 0xF0) {
+            if (text >= FIRST_FORUBYTE) {
                 count = 4;
-            } else if(text >= 0xE0) {
+            } else if (text >= FIRST_THREEBYTE) {
                 count = 3;
             } else {
                 count = 2;
             }
             for (int i = 1; i < count; i++) {
                 text = fgetc(input);
-                if (text >= LIM && text < 0xC0 ) {
-                    *(result+i) = (unsigned char)text;
+                if (text >= LIM && text < FIRST_TWOBYTE) {
+                    *(result + i) = (unsigned char)text;
                 } else {
-                    ungetc(text, input):
+                    ungetc(text, input);
                     skip++;
                     break;
                 }
-             }
+            }
             if (skip == 1) {
                 continue;
             }
